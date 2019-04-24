e5f7aac5d624660e468176b0189b1e07449b2140
Alain Schlesser <alain.schlesser@gmail.com>
2019-04-24 20:54:00 +0200

    Update src/WP_CLI/Core/CoreUpgrader.php
    Co-Authored-By: wojsmol <wojsmol@wp.pl>

--- src/WP_CLI/Core/CoreUpgrader.php
+++ src/WP_CLI/Core/CoreUpgrader.php
@@ -24,7 +24,7 @@ class CoreUpgrader extends DefaultCoreUpgrader {
 	 *
 	 * @param string $package          The URI of the package. If this is the full path to an
 	 *                                 existing local file, it will be returned untouched.
-	 * @param bool   $check_signatures Whether to validate file signatures. Default false.
+	 * @param bool   $check_signatures Whether to validate file signatures. Default true.
 	 * @return string|WP_Error The full path to the downloaded package file, or a WP_Error object.
 	 */
 	public function download_package( $package, $check_signatures = true ) {
