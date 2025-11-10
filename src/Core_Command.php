7420d57d4b91fdc594a1f7aa0770ca2e99292910
Pascal Birchler <pascalb@google.com>
2025-11-10 15:57:40 +0100

    Lint fixes

--- src/Core_Command.php
+++ src/Core_Command.php
@@ -1269,16 +1269,14 @@ EOT;
 
 				WP_CLI::success( 'WordPress updated successfully.' );
 			}
-		} else {
 			// Check if user attempted to downgrade without --force
-			if ( ! empty( Utils\get_flag_value( $assoc_args, 'version' ) ) && version_compare( Utils\get_flag_value( $assoc_args, 'version' ), $wp_version, '<' ) ) {
-				// Requested version is older than current (downgrade attempt)
-				WP_CLI::log( "WordPress is up to date at version {$wp_version}." );
-				WP_CLI::log( "The version you requested (" . Utils\get_flag_value( $assoc_args, 'version' ) . ") is older than the current version ({$wp_version})." );
-				WP_CLI::log( "Use --force to update anyway (e.g., to downgrade to version " . Utils\get_flag_value( $assoc_args, 'version' ) . ")." );
-			} else {
-				WP_CLI::success( "WordPress is up to date at version {$wp_version}." );
-			}
+		} elseif ( ! empty( Utils\get_flag_value( $assoc_args, 'version' ) ) && version_compare( Utils\get_flag_value( $assoc_args, 'version' ), $wp_version, '<' ) ) {
+			// Requested version is older than current (downgrade attempt)
+			WP_CLI::log( "WordPress is up to date at version {$wp_version}." );
+			WP_CLI::log( 'The version you requested (' . Utils\get_flag_value( $assoc_args, 'version' ) . ") is older than the current version ({$wp_version})." );
+			WP_CLI::log( 'Use --force to update anyway (e.g., to downgrade to version ' . Utils\get_flag_value( $assoc_args, 'version' ) . ').' );
+		} else {
+			WP_CLI::success( "WordPress is up to date at version {$wp_version}." );
 		}
 	}
 
